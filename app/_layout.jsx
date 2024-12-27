ea1717cf27b3ee563447d3180781a41d1d27a701
onurvarsak <onurvarsak@outlook.com>
2024-12-27 14:38:08 +0300

    add Tabs.Screen to change pages names

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,7 +3,34 @@ import React from "react"
 import { Tabs } from "expo-router"
 
 const _layout = () => {
-  return <Tabs></Tabs>
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: "Home"
+        }}
+      />
+      <Tabs.Screen
+        name='explore'
+        options={{
+          title: "Explore"
+        }}
+      />
+      <Tabs.Screen
+        name='create'
+        options={{
+          title: "Create"
+        }}
+      />
+      <Tabs.Screen
+        name='profile'
+        options={{
+          title: "Profile"
+        }}
+      />
+    </Tabs>
+  )
 }
 
 export default _layout
